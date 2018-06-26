26fc58771d4cf769b8d652f09e63e8a9c018d2d3
Rey Tucker <git@ryantucker.us>
2018-06-26 17:28:20 -0400

    add #bot hashtag

--- scrobbletooter.py
+++ scrobbletooter.py
@@ -189,7 +189,7 @@ def main():
         t_artist_name = t_artist.get_name() if t_artist is not None else "?"
         t_track_name = t.get_title()
 
-        msg = "#NowPlaying in the #CatgirlFortress\n\n%s - \"%s\"\n\n#np %s" % (t_artist_name, t_track_name, t_url)
+        msg = "#NowPlaying in the #CatgirlFortress\n\n%s - \"%s\"\n\n#bot #np %s" % (t_artist_name, t_track_name, t_url)
 
         masto.status_post(msg, visibility = 'unlisted')
         countdown -= 1
